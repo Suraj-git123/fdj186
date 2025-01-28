3ebed538a4d595760961a91223d0e9aa822a261c
Suraj-git123 <ingolesuraj53@gmail.com>
2025-01-28 12:41:19 +0530

    added m4

--- GitDemo/src/com/app/Test.java
+++ GitDemo/src/com/app/Test.java
@@ -11,6 +11,9 @@ public class Test {
 	public void m3(){
 		System.out.println("Method m3()");
 	}	
+	public void m4(){
+		System.out.println("Method m4()");
+	}	
 
 	public static void main(String[] args) {
 		System.out.println("welcome to Github");
